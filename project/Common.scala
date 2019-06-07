1ee95c7932bac7609f2a8d2a9408775b378c2369
Mark Tranter <mark.a.tranter@gmail.com>
2019-06-07 14:19:26 +1000

    Update organisation

--- project/Common.scala
+++ project/Common.scala
@@ -4,6 +4,7 @@ import sbt.URL
 object Common {
   def apply() = Seq(
     scalaVersion := "2.12.8",
+    organization := "com.engitano",
     organizationName := "Engitano",
     startYear := Some(2019),
     licenses += ("MIT", new URL("http://opensource.org/licenses/MIT")),
